48687b95ff1a5dd5f4d3aaaf3cec88f08d7430a6
zhengsp <271252643@qq.com>
2018-09-08 23:45:23 +0800

    显示层data-xx自定义属性逻辑层event.currentTarget.dataset.xx来访问，驼峰命名法

--- pages/week/week.js
+++ pages/week/week.js
@@ -8,24 +8,28 @@ Page({
       "name": "投洽会明日开幕",
       "img": "http://img.xmnn.cn/003/002/790/00300279007_598912e6.jpg",
       "desc": "第二十届投洽会明日开幕 12万名客商共赴全球投资盛会",
-      "isRemed":false
+      "isRemed":false,
+      "id":66
     },
     "movies":[
       {
         "name": "会明日开幕",
         "img": "http://img.xmnn.cn/003/002/790/00300279007_598912e6.jpg",
         "desc": "第二十届投洽会明日开幕 12万名客商共赴全球投资盛会",
-        "isRemed": false
+        "isRemed": false,
+        "id": 77
       }, {
         "name": "第二十届开幕",
         "img": "http://img.xmnn.cn/003/002/790/00300279007_598912e6.jpg",
         "desc": "第二十届投洽会明日开幕 12万名客商共赴全球投资盛会",
-        "isRemed": true
+        "isRemed": true,
+        "id": 88
       }, {
         "name": "第二日开幕",
         "img": "http://img.xmnn.cn/003/002/790/00300279007_598912e6.jpg",
         "desc": "第二十届投洽会明日开幕 12万名客商共赴全球投资盛会",
-        "isRemed": true
+        "isRemed": true,
+        "id": 99
       }
     ],
     "score":68,
@@ -90,9 +94,24 @@ Page({
     
   },
 
+  /**
+   * 返回本周的事件处理函数
+   */
   f0:function(event){
     this.setData({
       currentIndex:this.data.movies.length-1
     });
+  },
+  
+  /**
+   * 幻灯片点击事件的处理函数
+   */
+  f1:function(event){
+    console.log(event.currentTarget)
+    var movieId = event.currentTarget.dataset.movieId;
+    console.log(movieId);
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + movieId,
+    })
   }
 })
